97c4ee4a163c4b5a0f60568b7f37bae408fb81ad
khadija qoudmanne <rizkilive@gmail.com>
2023-03-21 16:35:38 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,16 @@
 void print_alphabet_x10(void)
 
 {
-	char c;
-	int i = 0;
+char c;
+int i = 0;
 
-	while (i <= 9)
-	{
-	for (c = 'a'; c <= 'z'; c++)
-	{
-	_putchar(c);
-	}
-        _putchar('\n');
-	i++;
-	}
+while (i <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+i++;
+}
 }
