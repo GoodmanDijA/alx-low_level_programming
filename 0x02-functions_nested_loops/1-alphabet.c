7673ee719b35a97cafbe3907ad04d97ba797ae55
khadija qoudmanne <rizkilive@gmail.com>
2023-03-21 16:12:27 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ */
+
+void print_alphabet(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+
+_putchar('\n');
+}
