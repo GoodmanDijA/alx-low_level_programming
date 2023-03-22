f8ce54417b28890f23f4a71c2cade333402ef3b9
khadija qoudmanne <rizkilive@gmail.com>
2023-03-21 19:35:50 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,7 +31,7 @@ if (prod >= 100)
 _putchar((prod / 100) + '0');
 _putchar(((prod / 10)) % 10 + '0');
 }
-else if (prod <= 99 && prod >= 10 )
+else if (prod <= 99 && prod >= 10)
 {
 _putchar((prod / 10) + '0');
 }
